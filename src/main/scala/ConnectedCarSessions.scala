441dfbf44db424600fbc251785338f0205781546
Spencer Uresk <suresk@suresk-l.corp.instructure.com>
2017-04-14 12:23:04 -0600

    Adding connected car exercises

---
+++ src/main/scala/ConnectedCarSessions.scala
@@ -0,0 +1,49 @@
+import com.dataartisans.flinktraining.exercises.datastream_java.datatypes.{ConnectedCarEvent, GapSegment}
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.AssignerWithPunctuatedWatermarks
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.scala.function.WindowFunction
+import org.apache.flink.streaming.api.watermark.Watermark
+import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.util.Collector
+
+import scala.collection.JavaConverters._
+
+/**
+  * Created by suresk on 4/14/17.
+  */
+object ConnectedCarSessions {
+
+  class ConnectedCarAssigner(watermarkMillis: Long) extends AssignerWithPunctuatedWatermarks[ConnectedCarEvent] {
+    override def checkAndGetNextWatermark(lastElement: ConnectedCarEvent, extractedTimestamp: Long): Watermark = {
+      return new Watermark(extractedTimestamp - watermarkMillis)
+    }
+
+    override def extractTimestamp(element: ConnectedCarEvent, previousElementTimestamp: Long): Long = element.timestamp
+  }
+
+  class CreateGapSegment extends WindowFunction[ConnectedCarEvent, GapSegment, String, TimeWindow] {
+    override def apply(key: String, window: TimeWindow, input: Iterable[ConnectedCarEvent], out: Collector[GapSegment]): Unit = {
+      out.collect(new GapSegment(input.asJava))
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+
+    val carData = env.readTextFile("/Users/suresk/dev/flink-training/data/carInOrder.csv")
+
+    carData
+      .map(ConnectedCarEvent.fromString(_))
+      .assignTimestampsAndWatermarks(new ConnectedCarAssigner(25000))
+      .keyBy(_.carId)
+      .window(EventTimeSessionWindows.withGap(Time.seconds(15)))
+      .apply(new CreateGapSegment)
+      .print()
+
+    env.execute("Connected Car Sessions")
+  }
+}
